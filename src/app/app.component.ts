6e9b266c4e47856ed4f11fdae7817ca05f744d90
LuisaFGO <fgiraldo070@gmail.com>
2024-06-03 13:19:47 -0500

    created app to-do

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './page/calendar/calendar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CalendarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
